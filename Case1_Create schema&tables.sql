025d24a766bef147c8e914745ac83a1195664b86
hoiyurie <84236678+hoiyurie@users.noreply.github.com>
2023-02-26 20:45:06 +1300

    Update Case1_Create schema&tables.sql

--- Case1_Create schema&tables.sql
+++ Case1_Create schema&tables.sql
@@ -1,3 +1,8 @@
+/*
+Create schema and tables, insert values for the tables
+*/
+
+----------------------------------------------------
 CREATE SCHEMA dannys_diner;
 
 
@@ -51,3 +56,95 @@ INSERT INTO members
 VALUES
   ('A', '2021-01-07'),
   ('B', '2021-01-09');
+
+
+
+  ------------------------------------------------------------
+
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+-- 1. What is the total amount each customer spent at the restaurant?
+-- 2. How many days has each customer visited the restaurant?
+-- 3. What was the first item from the menu purchased by each customer?
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+-- 5. Which item was the most popular for each customer?
+-- 6. Which item was purchased first by the customer after they became a member?
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+--------------------------------------------------------------
+-- 1. What is the total amount each customer spent at the restaurant?
+
+
+-- create a subquery at first and name it 'sub'
+-- use created subquery INNER JOIN sales table 
+
+SELECT s.customer_id,SUM(sub.TotalSalesAmount) AS total_salesamount
+FROM (
+	SELECT 
+		s.customer_id, 
+		m.price, 
+		COUNT(s.order_date) AS quantity,
+		m.price* COUNT(s.order_date) AS TotalSalesAmount
+	FROM dbo.sales s
+	INNER JOIN dbo.menu m
+	ON s.product_id = m.product_id
+	GROUP BY customer_id,m.price
+	) sub
+
+INNER JOIN dbo.sales s
+ON sub.customer_id = s.customer_id
+GROUP BY s.customer_id
+
+----------------------------------------------------------------------
+-- 2. How many days has each customer visited the restaurant?
+
+
+-- use CAST function to combine Month and Day and change it to string from integer
+-- DISTINCT reduce duplicate 
+
+SELECT 
+customer_id,
+COUNT(DISTINCT(CAST(MONTH(order_date) AS VARCHAR)+CAST(DAY(order_date) AS VARCHAR))) AS times
+FROM dbo.sales
+GROUP BY customer_id
+
+SELECT *
+FROM dbo.sales s
+INNER JOIN dbo.menu m
+ON s.product_id = m.product_id
+
+--------------------------------------------------------------------------
+-- 3. What was the first item from the menu purchased by each customer?
+
+-- used ROW_NUMBER to create index for each customer
+--create CTE 
+-- fetch first row of each customer ( where RowNumberEachCustomer=1) 
+
+WITH fetch_rows AS
+(
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS RowNumberEachCustomer
+	FROM 
+		(SELECT 
+			s.customer_id, 
+			m.product_name,
+			s.order_date
+		FROM [dbo].[sales] s
+			LEFT JOIN [dbo].[menu] m
+				ON s.product_id = m.product_id
+
+		) AS sales_menu
+)
+
+SELECT *
+FROM fetch_rows
+WHERE RowNumberEachCustomer = 1;
+
+----------------------------------------------------------------------------------
+
+--4. 
